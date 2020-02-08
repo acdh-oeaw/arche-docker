cd498813c7ac3c139804431749b0615aaada8183
Mateusz Żółtak <zozlak@zozlak.org>
2020-02-08 20:20:29 +0100

    Init scripts enhancements: * check if a transaction with the repository can be opened instead of waiting a fixed amount of time * prefix init scripts with numbers to assure proper execution order

--- root/home/www-data/initScripts.sh
+++ root/home/www-data/initScripts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# TODO - wait for the whole app stack to go up in the right way
-sleep 10
+while [ "`curl -i http://127.0.0.1/transaction -X POST 2>/dev/null | grep -c '201 Created'`" != "1" ]; do
+    echo "Waiting for services to be up..."
+    sleep 1
+done
+sleep 1
 for i in `ls -1 /home/www-data/config/initScripts`; do
    echo -e "##########\n# Running $i\n##########\n"
    /home/www-data/config/initScripts/$i
